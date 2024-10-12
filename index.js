422c2565eeab45600c1c7baf8390143864b1ac81
MeowyCats2 <136881097+MeowyCats2@users.noreply.github.com>
2024-10-12 09:54:33 -0700

    Make checker use process.env.checkURL

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let parser = new Parser();
 let firstItem = null
 setInterval(async () => {
   try {
-    let feed = await parser.parseURL('https://mijoviewerswar.miraheze.org/wiki/Special:RecentChanges?feed=rss');
+    let feed = await parser.parseURL(process.env.checkURL);
     if (feed.items.length === 0) return;
     if (firstItem !== null && firstItem !== feed.items[0].link) {
       const item = feed.items[0]
